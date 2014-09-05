905267494abfffdc70f4b87b8db9727ff12f5b82
Michael Shaffer <shafferm@shafferm.ucdenver.pvt>
2014-09-05 10:23:34 -0600

    Refactored out get_reactions method, now uses get_ko2rxns from parse_reaction.py

--- predict_reactions.py
+++ predict_reactions.py
@@ -11,75 +11,7 @@ from functools import partial
 import dict_manips
 import numpy
 import argparse
-
-def get_reactions():
-    """get compounds for each reaction and each KO"""
-    f = open("reaction", 'U')
-    f = f.read()
-    f = f.strip().split('///')
-    pathway2ko = dict()
-    ko2rxn = dict()
-    rxn2co = dict()
-    for entry in f:
-        i = 0
-        entry = entry.strip().split('\n')
-        kos = []
-        paths = []
-        hasOrtho = False
-        
-        while i < len(entry):
-            rev = False
-            new_start = entry[i][:12].strip()
-            line = entry[i][12:].strip()
-
-            if new_start == "ENTRY":
-                r = line.strip().split()[0]
-                start = "ENTRY"
-            elif new_start == "EQUATION":
-                equ = line.split('=>')
-                if equ[0][-1] == '<':
-                    rev = True
-                    equ[0] = equ[0][:-1]
-                reacts = list()
-                for part in equ[0].strip().split():
-                    if part[0] == 'C' or part[0] == 'G':
-                        reacts.append(part[:6])
-                prods = list()
-                for part in equ[1].strip().split():
-                    if part[0] == 'C' or part[0] == 'G':
-                        prods.append(part[:6])
-                start = "EQUATION"
-            elif new_start == "PATHWAY":
-                paths.append(line.strip().split()[0][-5:])
-                start = "PATHWAY"
-            elif new_start == "ORTHOLOGY":
-                kos.append(line.strip().split()[0])
-                hasOrtho = True
-                start = "ORTHOLOGY"
-            elif new_start == "":
-                if start == "ORTHOLOGY":
-                    kos.append(line.strip().split()[0])
-                if start == "PATHWAY":
-                    paths.append(line.strip().split()[0][-5:])
-            else:
-                start = new_start
-            i+=1
-        if hasOrtho == True:
-            if len(r) != 6 and r.startswith('R') == False:
-                print r
-            for ko in kos:
-                if ko in ko2rxn:
-                    ko2rxn[ko].add(r)
-                else:
-                    ko2rxn[ko] = set([r])
-            for path in paths:
-                if path in pathway2ko:
-                    pathway2ko[path] = pathway2ko[path] | set(kos)
-                else:
-                    pathway2ko[path] = set(kos)
-            rxn2co[r] = reacts,prods,rev
-
-    return pathway2ko,ko2rxn,rxn2co
+import parse_reaction
     
 def collapse_to_reactions(table, ko2rxns):
     #get dictionary of rxns to kos
@@ -106,26 +38,26 @@ def collapse_to_reactions(table, ko2rxns):
     data = numpy.array([x[1] for x in merged_rxns], dtype = float)
     return Table(data, [x[0] for x in merged_rxns], table.ids(), type="OTU table")
 
-def main(input_file, output_file):
-    #get reactions
-    pathway2ko, ko2rxn, rxn2co = get_reactions()
-    
+def main(input_file, output_file, classic):
     #parse biom table
     table = load_table(input_file)
 
     #combine rows of KO's from the same reaction
-    table = collapse_to_reactions(table, ko2rxn)
-        
-    #print biom table
-    table.to_json("predict_reactions.py", open(output_file, 'w'))
+    table = collapse_to_reactions(table, parse_reaction.get_ko2rxns())
     
-    #print to tab delimited biom table
-    #f = open(output_file, 'w')
-    #f.write(table.to_tsv())
+    if classic:
+        #print to tab delimited biom table
+        f = open(output_file, 'w')
+        f.write(table.to_tsv())
+    else:
+        #print biom table
+        table.to_json("predict_reactions.py", open(output_file, 'w'))
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="location of gene biom table")
     parser.add_argument("-o", "--output", help="name of new reaction biom table")
+    parser.add_argument("-b", "--to_classic_table", action="store_true", 
+        help="output as a tab delimited table")
     args = parser.parse_args()
-    main(args.input, args.output)
+    main(args.input, args.output, args.to_classic_table)
